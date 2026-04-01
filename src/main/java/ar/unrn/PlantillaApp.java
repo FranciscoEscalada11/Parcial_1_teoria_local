7fbac3042a5d4042b7bb75ba169d351f7828b0e9
franescalada <franescalada11@gmail.com>
2026-04-01 18:50:14 -0300

    Documentacion

--- src/main/java/ar/unrn/PlantillaApp.java
+++ src/main/java/ar/unrn/PlantillaApp.java
@@ -9,7 +9,19 @@ import java.util.Arrays;
  */
 public class PlantillaApp {
 
-    /**/
+    /*
+    * Recorre dos arreglos de String convirtiendo cada elemento de los arreglos en enteros y asi al dividirlos
+    * obtiene un valor decodificado, este formara parte del arreglo de String de salida
+    * @param lecturas Arreglo de string a recorrer
+    * @param factores Arreglo de string a recorrer
+    * @pre lecturas y factores no pueden ser null
+    * @return salida un Arreglo de strings con los valores decodificados o mensajes de error correspondientes
+    * @post Si la capacidad pedida para el arreglo de salida supera la capacidad de alguno de los arreglos de entrada
+    * la fila correspondiente estara completa con el mensaje de error ERROR_INDICE
+    * @post Si alguno de los Strings es null, la fila correspondiente del arreglo de salida mostrara el mensaje de
+    * error ERROR_NULO
+    * @post Si al momento de la decodificacion el valor del arreglo factores es 0, este se posicionara como divisor
+    * por lo que en la fila del arreglo de salida correspondiente se vera el mensaje de error ERROR_DIV_ZERO*/
     public static String[] normalizarTelemetria(String[] lecturas, String[] factores, int capacidad){
         int lLecturas = lecturas.length;
         int lFactores = lecturas.length;
@@ -18,7 +30,7 @@ public class PlantillaApp {
         for(int i = 0; i < capacidad; i++){
             try{
                 if(i > lLecturas || i > lFactores){
-                    throw new IndexOutOfBoundsException("Alguno de los arreglos es menor a la capacidad pedidad")
+                    throw new IndexOutOfBoundsException("Alguno de los arreglos es menor a la capacidad pedidad");
                 }
             }catch (IndexOutOfBoundsException e){
                 System.out.println("ERR_INDICE" + e.getMessage());
