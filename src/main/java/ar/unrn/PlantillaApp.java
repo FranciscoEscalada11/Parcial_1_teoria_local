79efee5bec57b1652eb4444bf8767a726f11c954
franescalada <franescalada11@gmail.com>
2026-04-01 18:28:02 -0300

    Preparacion de metodo

--- src/main/java/ar/unrn/PlantillaApp.java
+++ src/main/java/ar/unrn/PlantillaApp.java
@@ -1,5 +1,6 @@
 package ar.unrn;
-
+import java.util.Scanner;
+import java.util.Arrays;
 /**
  * Esta es una plantilla de main.
  * Usen Shift+F6 sobre el nombre para cambiarlo al que necesiten.
@@ -7,6 +8,42 @@ package ar.unrn;
  * Acá va la consigna del ejercicio.
  */
 public class PlantillaApp {
+
+    /**/
+    public static String[] normalizarTelemetria(String[] lecturas, String[] factores, int capacidad){
+        int lLecturas = lecturas.length;
+        int lFactores = lecturas.length;
+        String[] salida = new String[capacidad];
+
+        for(int i = 0; i < capacidad; i++){
+            try{
+                if(i > lLecturas || i > lFactores){
+                    throw new IndexOutOfBoundsException("Alguno de los arreglos es menor a la capacidad pedidad")
+                }
+            }catch (IndexOutOfBoundsException e){
+                System.out.println("ERR_INDICE" + e.getMessage());
+            }
+            try{
+                if (lecturas[i] == null || factores[i] == null){
+                    throw new NullPointerException("La cadena evaluada es NULL");
+                }
+            } catch (NullPointerException e) {
+                //asignacion
+                salida[i] = "ERR_NULO";
+            }
+            int nLecturas = Integer.parseInt(lecturas[i]);
+            int nFactores = Integer.parseInt(factores[i]);
+            try{
+                int normalizado = nLecturas / nFactores;
+                String fila = Integer.toString(normalizado);
+                salida[i] = fila;
+            } catch (ArithmeticException e){
+                System.out.println("ERR_DIV_ZERO" + e.getMessage());
+                salida[i] = "ERR_DIV_ZERO";
+            }
+        }
+        return salida;
+    }
 
     /**
      * Punto de entrada del ejercicio.
